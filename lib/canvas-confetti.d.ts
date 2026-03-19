016d81ac79412396bca43779c4718a70eb46daee
ugbeadie <ugbeadie3@gmail.com>
2026-03-19 15:48:55 +0100

    fix: trying to resolve build error

---
+++ lib/canvas-confetti.d.ts
@@ -0,0 +1,11 @@
+declare module "canvas-confetti" {
+  interface ConfettiOptions {
+    particleCount?: number;
+    spread?: number;
+    origin?: { x?: number; y?: number };
+  }
+
+  function confetti(options?: ConfettiOptions): void;
+
+  export default confetti;
+}
